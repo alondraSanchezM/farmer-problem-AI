3e466ec00a56be08efe83a42e319844dd1aba7e5
alo_sanchez <alondra1d35@gmail.com>
2021-05-08 00:51:12 -0500

    :sparkles: UI validation

--- problemagranjero/ProblemaGranjeroMain.java
+++ problemagranjero/ProblemaGranjeroMain.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class ProblemaGranjeroMain implements ActionListener {
 
@@ -31,68 +32,78 @@ public class ProblemaGranjeroMain implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent evento) {
         if (ui.getBtn_busAnchura() == evento.getSource()) {
-            
+
             Estado eInicial = new Estado(conversor(ui.getCb_granjeroIni().getSelectedItem().toString()),
-                                        conversor(ui.getCb_loboIni().getSelectedItem().toString()),
-                                        conversor(ui.getCb_cabraIni().getSelectedItem().toString()),
-                                        conversor(ui.getCb_colIni().getSelectedItem().toString()));
-            
+                    conversor(ui.getCb_loboIni().getSelectedItem().toString()),
+                    conversor(ui.getCb_cabraIni().getSelectedItem().toString()),
+                    conversor(ui.getCb_colIni().getSelectedItem().toString()));
+
             Estado eFinal = new Estado(conversor(ui.getCb_granjeroFin().getSelectedItem().toString()),
-                                        conversor(ui.getCb_loboFin().getSelectedItem().toString()),
-                                        conversor(ui.getCb_cabraFin().getSelectedItem().toString()),
-                                        conversor(ui.getCb_colFin().getSelectedItem().toString()));
+                    conversor(ui.getCb_loboFin().getSelectedItem().toString()),
+                    conversor(ui.getCb_cabraFin().getSelectedItem().toString()),
+                    conversor(ui.getCb_colFin().getSelectedItem().toString()));
+            if (eInicial.comparar(eFinal)) {
+                JOptionPane.showMessageDialog(null, "El estado inicial es el mismo que el estado final", "Error", JOptionPane.ERROR_MESSAGE);
+            } else if (!eInicial.esEstadoValido() || !eFinal.esEstadoValido()) {
+                JOptionPane.showMessageDialog(null, "Estados no permitidos", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                LinkedList resultadoAnchura = b.BFS(eInicial, eFinal);
 
-            LinkedList resultadoAnchura = b.BFS(eInicial, eFinal);
+                System.out.println("Recorrido busqueda por anchura: ");
+                for (int i = 0; i < resultadoAnchura.size(); i++) {
+                    System.out.println(i + 1 + "  " + resultadoAnchura.get(i));
+                }
 
-            System.out.println("Recorrido busqueda por anchura: ");
-            for (int i = 0; i < resultadoAnchura.size(); i++) {
-                System.out.println(i + 1 + "  " + resultadoAnchura.get(i));
-            }
+                ArrayList<Estado> caminoAnchura = b.caminoSolucion(resultadoAnchura);
 
-            ArrayList<Estado> caminoAnchura = b.caminoSolucion(resultadoAnchura);
-
-            for (Estado estado : caminoAnchura) {
-                System.out.println(estado.toString());
+                for (Estado estado : caminoAnchura) {
+                    System.out.println(estado.toString());
+                }
             }
 
         } else if (ui.getBtn_busProfundidad() == evento.getSource()) {
             Estado eInicial = new Estado(conversor(ui.getCb_granjeroIni().getSelectedItem().toString()),
-                                        conversor(ui.getCb_loboIni().getSelectedItem().toString()),
-                                        conversor(ui.getCb_cabraIni().getSelectedItem().toString()),
-                                        conversor(ui.getCb_colIni().getSelectedItem().toString()));
-            
+                    conversor(ui.getCb_loboIni().getSelectedItem().toString()),
+                    conversor(ui.getCb_cabraIni().getSelectedItem().toString()),
+                    conversor(ui.getCb_colIni().getSelectedItem().toString()));
+
             Estado eFinal = new Estado(conversor(ui.getCb_granjeroFin().getSelectedItem().toString()),
-                                        conversor(ui.getCb_loboFin().getSelectedItem().toString()),
-                                        conversor(ui.getCb_cabraFin().getSelectedItem().toString()),
-                                        conversor(ui.getCb_colFin().getSelectedItem().toString()));
+                    conversor(ui.getCb_loboFin().getSelectedItem().toString()),
+                    conversor(ui.getCb_cabraFin().getSelectedItem().toString()),
+                    conversor(ui.getCb_colFin().getSelectedItem().toString()));
 
-            LinkedList resultadoProfundidad = b.DFS(eInicial, eFinal);
+            if (eInicial.comparar(eFinal)) {
+                JOptionPane.showMessageDialog(null, "El estado inicial es el mismo que el estado final", "Error", JOptionPane.ERROR_MESSAGE);
+            } else if (!eInicial.esEstadoValido() || !eFinal.esEstadoValido()) {
+                JOptionPane.showMessageDialog(null, "Estados no permitidos", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                LinkedList resultadoProfundidad = b.DFS(eInicial, eFinal);
+                System.out.println("Recorrido busqueda por profundidad: ");
+                for (int i = 0; i < resultadoProfundidad.size(); i++) {
+                    System.out.println(i + 1 + "  " + resultadoProfundidad.get(i));
+                }
 
-            System.out.println("Recorrido busqueda por profundidad: ");
-            for (int i = 0; i < resultadoProfundidad.size(); i++) {
-                System.out.println(i + 1 + "  " + resultadoProfundidad.get(i));
-            }
+                ArrayList<Estado> caminoProfundidad = b.caminoSolucion(resultadoProfundidad);
 
-            ArrayList<Estado> caminoProfundidad = b.caminoSolucion(resultadoProfundidad);
-
-            for (Estado estado : caminoProfundidad) {
-                System.out.println(estado.toString());
+                for (Estado estado : caminoProfundidad) {
+                    System.out.println(estado.toString());
+                }
             }
 
         } else if (ui.getBtn_Reiniciar() == evento.getSource()) {
-            
+
         }
 
     }
-    
+
     //Convierte el string recibido del comboBox a un valor entero para el manejo del nodo Estado
-    private int conversor(String comboInfo){
-        if("Izquierda".equals(comboInfo)){
+    private int conversor(String comboInfo) {
+        if ("Izquierda".equals(comboInfo)) {
             return 1;
-        }else{
+        } else {
             return 2;
         }
-        
+
     }
 
 }
